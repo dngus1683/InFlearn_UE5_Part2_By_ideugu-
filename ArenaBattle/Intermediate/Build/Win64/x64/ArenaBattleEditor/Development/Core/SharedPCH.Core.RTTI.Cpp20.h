8decf808baa32165cbf17412a91159ef47718b34
Jangwh <dngus1683@naver.com>
2025-01-15 23:18:46 +0900

    gitignore 정리

--- ArenaBattle/Intermediate/Build/Win64/x64/ArenaBattleEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "D:/Study/InFlearn_UE5_Part2_By_ideugu/ArenaBattle/Intermediate/Build/Win64/x64/ArenaBattleEditor/Development/Core/SharedDefinitions.Core.RTTI.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- ArenaBattle/Intermediate/Build/Win64/x64/ArenaBattleEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "D:/Study/InFlearn_UE5_Part2_By_ideugu/ArenaBattle/Intermediate/Build/Win64/x64/ArenaBattleEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
