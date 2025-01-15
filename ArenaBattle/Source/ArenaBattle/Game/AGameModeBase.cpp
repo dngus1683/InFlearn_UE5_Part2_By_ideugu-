c14f719afea1411cdf1ec3e345b2cc2d70292a73
Jangwh <dngus1683@naver.com>
2025-01-16 02:51:01 +0900

    Step 2 study
    캐릭터와 입력 시스템

--- ArenaBattle/Source/ArenaBattle/Game/AGameModeBase.cpp
+++ ArenaBattle/Source/ArenaBattle/Game/AGameModeBase.cpp
@@ -5,12 +5,18 @@
 
 AAGameModeBase::AAGameModeBase()
 {
-	// 에디터 -> Content Browser -> 서드펄슨 클래스는 블루프린트로 제작되었기 때문에, 해당 블루프린트 우클릭 -> copy Reference를 통해 해당 애셋의 경로를 가져울 수 있음. 
+	// 에디터 -> Content Browser -> ThirdPerson 클래스는 블루프린트로 제작되었기 때문에, 해당 블루프린트 우클릭 -> copy Reference를 통해 해당 애셋의 경로를 가져울 수 있음. 
 	// 이때 클래스 정보만 가져오기 위해, 경로 뒤에 "_C"를 추가.
-	static ConstructorHelpers::FClassFinder<APawn> ThirdPersonClassRef(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C"));
+	/*static ConstructorHelpers::FClassFinder<APawn> ThirdPersonClassRef(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C"));
 	if (ThirdPersonClassRef.Class)
 	{
 		DefaultPawnClass = ThirdPersonClassRef.Class;
+	}*/
+
+	static ConstructorHelpers::FClassFinder<APawn> DefaultPawnClassRef(TEXT("/Script/ArenaBattle.ABCharacterPlayer"));
+	if (DefaultPawnClassRef.Class)
+	{
+		DefaultPawnClass = DefaultPawnClassRef.Class;
 	}
 
 	// ABPlayerController 헤더를 인클루드하여 직접적으로 PlayerControllerClass = AABPlayerController::StaticClass();를 통해 지정할 수 있지만, 
