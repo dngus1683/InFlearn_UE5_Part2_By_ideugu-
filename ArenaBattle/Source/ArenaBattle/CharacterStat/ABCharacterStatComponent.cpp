c4d0611d436ed92f21a277b8b591d8c6cad0790d
Jangwh <dngus1683@naver.com>
2025-01-29 22:38:03 +0900

    Step 7 study

---
+++ ArenaBattle/Source/ArenaBattle/CharacterStat/ABCharacterStatComponent.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterStat/ABCharacterStatComponent.h"
+
+// Sets default values for this component's properties
+UABCharacterStatComponent::UABCharacterStatComponent()
+{
+	MaxHp = 200.f;
+	CurrentHp = MaxHp;
+}
+
+
+// Called when the game starts
+void UABCharacterStatComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetHp(MaxHp);
+	
+}
+
+float UABCharacterStatComponent::ApplyDamage(float InDamage)
+{
+	const float PrevHp = CurrentHp;
+	const float ActualDamage = FMath::Clamp<float>(InDamage, 0, InDamage);
+
+	SetHp(FMath::Clamp<float>(PrevHp - ActualDamage, 0.f, MaxHp));
+	if (CurrentHp <= KINDA_SMALL_NUMBER)
+	{
+		OnHpZero.Broadcast();
+	}
+
+	return ActualDamage;
+}
+
+void UABCharacterStatComponent::SetHp(float NewHp)
+{
+	CurrentHp = FMath::Clamp<float>(NewHp, 0, MaxHp);
+	
+	OnHpChanged.Broadcast(CurrentHp);
+}
+
---
+++ ArenaBattle/Source/ArenaBattle/UI/ABHpBarWidget.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/ABHpBarWidget.h"
+#include "Components/ProgressBar.h"
+#include "Interface/ABCharacterWidgetInterface.h"
+
+UABHpBarWidget::UABHpBarWidget(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	MaxHp = -1.f;
+}
+
+void UABHpBarWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	HpProgressBar = Cast<UProgressBar>(GetWidgetFromName(TEXT("PbHpBar")));
+	ensure(HpProgressBar);
+
+	IABCharacterWidgetInterface* CharacterWidget = Cast<IABCharacterWidgetInterface>(OwningActor);
+	if (CharacterWidget)
+	{
+		CharacterWidget->SetupCharacterWidget(this);
+	}
+}
+
+void UABHpBarWidget::UpdateHpBar(float NewCurrentHp)
+{
+	ensure(MaxHp > 0.f);
+	if (HpProgressBar)
+	{
+		HpProgressBar->SetPercent(NewCurrentHp / MaxHp);
+	}
+}
