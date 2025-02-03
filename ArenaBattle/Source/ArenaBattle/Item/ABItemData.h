82c172bf632db722d3b1eca5c056c4cc88fc8964
Jangwh <dngus1683@naver.com>
2025-02-03 23:46:34 +0900

    Step 9 study

--- ArenaBattle/Source/ArenaBattle/Item/ABItemData.h
+++ ArenaBattle/Source/ArenaBattle/Item/ABItemData.h
@@ -22,6 +22,12 @@ UCLASS()
 class ARENABATTLE_API UABItemData : public UPrimaryDataAsset
 {
 	GENERATED_BODY()
+
+public:
+	FPrimaryAssetId GetPrimaryAssetId() const override
+	{
+		return FPrimaryAssetId("ABItemData", GetFName());
+	}
 	
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Type)
